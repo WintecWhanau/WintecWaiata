716ea0eb915321a53aef7a287d1649650a5b4412
JonSun30 <jon.sun30@gmail.com>
2019-11-19 20:24:53 +1300

    Carvins

--- wintec_waiata/lib/shared/Info.dart
+++ wintec_waiata/lib/shared/Info.dart
@@ -6,9 +6,13 @@ class InfoString {
       'Comprising of three main buildings and seven significant carvings, the design of the marae depicts a contemporary version of traditional aspects with a focus on matauranga Māori (learning) and tikanga (customs and traditions)';
 
   static final String protocolTitle = 'Marae Protocols';
+
   static final String aboutUsTitle = 'Wintec Waiata App';
+
   static final String protocolImage = 'assets/images/image2.jpg';
+
   static final String wintecLogo = 'assets/images/Logo.jpg';
+
   static final String protocols =
       'Te Kōpū Mānia o Kirikiriroa is a functional, modern marae complex set up to accommodate a variety of uses.\n'
       'Although our marae is contemporary, we still have protocols and tikanga (customs and traditions) to adhere to.';
@@ -19,7 +23,6 @@ class InfoString {
       '• Do not sit on pillows or stand on mattresses\n'
       '• Please do not leave hats, hair brushes or combs on table tops\n'
       '• Refrain from hanging items on carvings';
-
   static final String wharekai = '• Karakia (grace) is said before every meal\n'
       '• Never sit on tables\n'
       '• Seats are provided for your convenience\n'
@@ -34,15 +37,65 @@ class InfoString {
 
   static final String appDescription =
       'The Wintec Waiata App is a mobile app that helps people learn Wintec songs and Wintec Marae.';
+
   static final String groupName = 'Te Whānau';
+
   static final String gorupInfo = 'GROUP LEADER - Jon\n'
       'UX/UI DESIGN - Jon, Toni, Stevie\n'
       'APP PROGRAMMING - Stevie, Jon, Toni\n'
       'MULTIMEDIA - James & Elysia\n'
       'DATA COLLECTION - Elysia\n';
+
   static final String disclaimTitle = 'Disclaimer';
   static final String disclaimer =
       'This Wintec Waiata app is a prototype version, created for Wintec staff and students. '
       'It is intended that staff and students shall use this app to learn Wintec waiata and gain a greater understanding of the Wintec Marae, Te Kōpū Mānia o Kirikiriroa. '
       'Waiata or information on the Marae may change accordingly, making older versions redundant – Wintec takes no responsibility for outdated information contained within the app. Kia ora.';
+
+  static final String memorialPillar = 'Waka Maumahara';
+  static final String memorialPillarImage = 'assets/images/mainPillar.jpg';
+  static final String memorialPillarDesc =
+      'On the marae ātea (courtyard) is 7.5 metre pou (pillar) in the form of a waka (canoe).\n'
+      'This is the waka maumahara (memorial pillar) which faces Taupiri mountain.'
+      'At the top of the waka maumahara stands the native kaahu (hawk) representing the Māori Queen Dame Te Atarirangikaahu.';
+
+  static final String pillar = 'Pou Whakarae';
+  static final String pillarImage = 'assets/images/pillar.jpg';
+  static final String pillarDesc =
+      'On the marae ātea (courtyard) is 7.5 metre pou (pillar) in the form of a waka (canoe).\n'
+      'This is the waka maumahara (memorial pillar) which faces Taupiri mountain.'
+      'At the top of the waka maumahara stands the native kaahu (hawk) representing the Māori Queen Dame Te Atarirangikaahu.';
+
+  static final String internalWharenui = 'Pou-tūā-rangi';
+  static final String internalWharenuiDesc =
+      'Near the waka maumahara (memorial pillar ) are seven smaller pou whakarae(pillars) which symbolise the star cluster Matariki (Pleides). This star cluster is significant to Māori and Tainui culture.';
+
+  static final String internalWharenuiDescImage =
+      'assets/images/internalWharenui.jpg';
+
+  static final String internalWharenui1 = 'Pou-tūā-rongo - Tawhaki';
+  static final String internalWharenuiDesc1 =
+      'Located on the back wall of the wharenui the pou-tūā-rongo is a carving of Tawhaki who received the baskets of knowledge (nga kete wananga).\n'
+      'Tawhaki is featured looking towards the doorway and the world of light. This was also carved in totara from a tree which use to stand on Wintec’s city campus.\n';
+  static final String internalWharenuiDescImage1 =
+      'assets/images/internalWharenui1.jpg';
+
+  static final String gate = 'Tomokanga';
+  static final String gateDesc =
+      'The entrance designs welcome people from around the world of Wintec.\n'
+      'The sub-tribes (hapū) of this area are shown welcoming visitors onto the marae in a central carving at the entrance.'
+      'Mātauranga Māori and part of the creation story of Tainui are also represented in the carvings.';
+  static final String gateImage = 'assets/images/gate1.jpg';
+
+  static final String doorwayLintels = 'Pare and Whakawae';
+  static final String doorwayLintelsDesc =
+      'Above the wharenui pare (doorway) are three figures nga kete wānanga representing the three baskets of knowledge. Part of the creation story of Tainui is also told in the designs.\n'
+      'Manaia and takarangi patterns symbolise growth and development. The textured ngao matariki pattern on the sides of the doorway (whakawae) represents the welcoming of the cultures and peoples of the world.';
+  static final String doorwayLintelsImage = 'assets/images/doorwayLintels.jpg';
+
+  static final String windowLintels = 'Kōrupe';
+  static final String windowLintelsDesc =
+      'The taniwha Tuheitia is carved on the window lintel (kōrupe) above the front window of Te Kākano a te Kaahu, along with ngao matariki and manaia designs.\n'
+      'Tuheitia holds guardianship of the Waipa river and was regarded with awe and respect.';
+  static final String windowLintelsImage = 'assets/images/windowLintels.jpg';
 }
